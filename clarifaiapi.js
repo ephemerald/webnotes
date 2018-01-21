d8977baa37533069b87f51bf7e1f6a84a68f2537
nvillegas <nvillegasx@gmail.com>
2018-01-20 21:18:11 -0800

    update getting api file, waiting for replacement image code

--- clarifaiapi.js
+++ clarifaiapi.js
@@ -7,12 +7,14 @@ const app = new Clarifai.App({
 
 console.log('hello');
 
+
 app.models.predict(Clarifai.NSFW_MODEL, "http://cdn2-www.dogtime.com/assets/uploads/2011/01/file_23244_what-is-the-appenzeller-sennenhunde-dog-300x189.jpg").then(
   function(response) {
       console.log(response);
       console.log(response.outputs[0].data);
-      // if (nsfw > 0.5) {
-      //       var doggoIndex = Math.floor(Math.random() * doggoArray.length);
-      //       iamges[i].src = doggoArray[doggoIndex];
+      var sfwVal = response.outputs[i].data.concepts[1].value;
+      if (sfwVal > 0.5) {
+      	//their part
+      }
       });
     
